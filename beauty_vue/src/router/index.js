57ee667975874ad09e756d90ff00b5aea33ccd57
LiganiumInc <liganbernardin@gmail.com>
2022-03-31 21:30:01 +0000

    first commit

---
+++ beauty_vue/src/router/index.js
@@ -0,0 +1,93 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import store from '../store'
+
+import HomeView from '../views/HomeView.vue'
+
+import Product from '../views/Product.vue'
+import Category from '../views/Category.vue'
+import Search from '../views/Search.vue'
+import Cart from '../views/Cart.vue'
+import SignUp from '../views/SignUp.vue'
+import Login from '../views/Login.vue'
+import MyAccount from '../views/MyAccount.vue'
+import Checkout from '../views/Checkout.vue'
+ 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about', 
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/:category_slug/:product_slug/',
+    name: 'Product',
+    component: Product
+  },
+  {
+    path: '/:category_slug',
+    name: 'Category',
+    component: Category
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: Search
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/log-in',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
+  },
+  {
+    path: '/cart/checkout',
+    name: 'Checkout',
+    component: Checkout,
+    meta: { 
+      requireLogin: true
+    }
+  },
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next({ name: 'LogIn', query: { to: to.path } });
+  } else {
+    next()
+  }
+})
+
+export default router
+  
---
+++ beauty_vue/src/views/MyAccount.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="page-my-account">
+        <div class="columns is-multiline">
+            <div class="column is-12">
+                <h1 class="title">My account</h1>
+            </div>
+
+            <div class="column is-12">
+                <button @click="logout()" class="button is-danger">Log out</button>
+            </div>
+
+            <hr>
+
+            
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+
+    name: 'MyAccount',
+    
+   
+    methods: {
+        logout() {
+            axios.defaults.headers.common["Authorization"] = ""
+            localStorage.removeItem("token")
+            localStorage.removeItem("username")
+            localStorage.removeItem("userid")
+            this.$store.commit('removeToken')
+            this.$router.push('/')
+        },
+      
+    }
+
+}
+</script>
